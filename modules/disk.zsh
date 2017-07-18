11a60ddd30e8f891ada271955de52ada0a582cbb
indrajit <eendroroy@gmail.com>
2017-07-18 17:59:48 +0600

    update disk module

--- modules/disk.zsh
+++ modules/disk.zsh
@@ -1,21 +1,21 @@
 #!/usr/bin/env zsh
 
-plib_p_name(){
+plib_disk_name(){
   echo -ne "`df -h . | tail -1 | awk '{print $1}'`";
 }
 
-plib_p_size(){
+plib_disk_size(){
   echo -ne "`df -h . | tail -1 | awk '{print $2}'`";
 }
 
-plib_p_used(){
+plib_disk_used(){
   echo -ne "`df -h . | tail -1 | awk '{print $3}'`";
 }
 
-plib_p_avail(){
+plib_disk_avail(){
   echo -ne "`df -h . | tail -1 | awk '{print $4}'`";
 }
 
-plib_p_cap(){
+plib_disk_cap(){
   echo -ne "`df -h . | tail -1 | awk '{print $5}'`";
 }
