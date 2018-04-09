fbdc246ac1329f1e960e549862fe8570f136c6b2
indrajit <eendroroy@gmail.com>
2018-04-10 00:37:51 +0600

    added php_major_version

--- modules/php.zsh
+++ modules/php.zsh
@@ -3,3 +3,7 @@
 plib_php_version(){
   which php > /dev/null && echo -ne `php -v | head -1 | awk -F'[ -]' '{print $2}' | tr -d ' \n'`
 }
+
+plib_php_major_version(){
+  which php > /dev/null && echo -ne `php -v | head -1 | awk -F'[ -.]' '{print $2}' | tr -d ' \n'`
+}
