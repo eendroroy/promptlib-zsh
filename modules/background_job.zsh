4707a952af6e77ef3e4d4d74f0e14e101c18d168
Louis Vinchon <louis.vinchon.dev@gmail.com>
2019-08-18 21:32:40 +0200

    [BC] Refactor plib_bg_count
    Remove formating.

--- modules/background_job.zsh
+++ modules/background_job.zsh
@@ -1,9 +1,5 @@
 #!/usr/bin/env zsh
 
 plib_bg_count() {
-  __jobc="`jobs | grep -v "pwd" | wc -l | tr -d ' '`";
-  if [[ "$__jobc" != 0 ]]; then
-    echo -ne "[$__jobc] "
-  fi
-  unset __jobc;
+  jobs | grep -v "pwd" | wc -l | tr -d ' \n'
 }
