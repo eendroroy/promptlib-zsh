fbe68eceb096cb2910ac0b336e8a9c071e271c1a
indrajit <eendroroy@gmail.com>
2023-12-19 12:39:01 +0600

    update crystal version lib

--- modules/crystal.zsh
+++ modules/crystal.zsh
@@ -4,22 +4,38 @@ plib_crystal_version(){
   command -v crystal > /dev/null && echo -ne "$(crystal -v | head -1 | awk '{print $2}')"
 }
 
+plib_crystal_major_version(){
+  command -v crystal > /dev/null && echo -ne "$(crystal -v | head -1 | awk -F'[ .]' '{print $2}')"
+}
+
+plib_crystal_minor_version(){
+  command -v crystal > /dev/null && echo -ne "$(crystal -v | head -1 | awk -F'[ .]' '{print $3}')"
+}
+
 plib_crystal_major_minor_version(){
   command -v crystal > /dev/null && echo -ne "$(crystal -v | head -1 | awk -F'[ .]' '{print $2"."$3}')"
 }
 
-plib_crystal_major_version(){
-  command -v crystal > /dev/null && echo -ne "$(crystal -v | head -1 | awk -F'[ .]' '{print $2}')"
+plib_crystal_patch_version(){
+  command -v crystal > /dev/null && echo -ne "$(crystal -v | head -1 | awk -F'[ .]' '{print $4}')"
 }
 
 plib_crenv_version(){
   command -v crenv > /dev/null && echo -ne "$(crenv version-name | awk '{print $1}')"
 }
 
+plib_crenv_major_version(){
+  command -v crenv > /dev/null && echo -ne "$(crenv version-name | awk -F. '{print $1}')"
+}
+
 plib_crenv_major_minor_version(){
   command -v crenv > /dev/null && echo -ne "$(crenv version-name | awk -F. '{print $1"."$2}')"
 }
 
-plib_crenv_major_version(){
-  command -v crenv > /dev/null && echo -ne "$(crenv version-name | awk -F. '{print $1}')"
+plib_crenv_minor_version(){
+  command -v crenv > /dev/null && echo -ne "$(crenv version-name | awk -F. '{print $2}')"
+}
+
+plib_crenv_patch_version(){
+  command -v crenv > /dev/null && echo -ne "$(crenv version-name | awk -F. '{print $3}')"
 }
