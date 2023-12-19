d99889bae3a537f0114046fa9c4886d79258aa15
indrajit <eendroroy@gmail.com>
2023-12-19 12:32:24 +0600

    add gradle version

---
+++ modules/gradle.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+plib_gradle_version(){
+  command -v vagrant > /dev/null && echo -ne "$(gradle -v | grep Gradle | awk '{print $2}')"
+}
+
+plib_gradle_major_version(){
+  command -v vagrant > /dev/null && echo -ne "$(gradle -v | grep Gradle | awk -F'[ .]' '{print $2}')"
+}
+
+plib_gradle_minor_version(){
+  command -v vagrant > /dev/null && echo -ne "$(gradle -v | grep Gradle | awk -F'[ .]' '{print $3}')"
+}
