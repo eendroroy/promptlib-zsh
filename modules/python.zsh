db7f39a76e3f29e8014552b245a40bc1f4f18044
Zachary Cutlip <uid000@gmail.com>
2024-08-23 11:15:08 -0700

    alternately use 'python3' if it exists and there is no 'python'

--- modules/python.zsh
+++ modules/python.zsh
@@ -1,19 +1,28 @@
 #!/usr/bin/env zsh
 
+# default is regular 'python'
+_PYTHON=python;
+if ! command -v python > /dev/null && command -v python3 > /dev/null;
+then
+    # if there *not* a 'python' but there *is* a 'python3', use that instead
+    # this is typical on macOS
+    _PYTHON=python3;
+fi
+
 plib_venv(){
   [[ -n "${VIRTUAL_ENV}" ]] && echo -n "($(basename "$VIRTUAL_ENV"))"
 }
 
 plib_python_version(){
-  command -v python > /dev/null && echo -ne "$(python --version 2>&1 | awk '{print $2}' | tr -d ' \n')"
+  command -v $_PYTHON > /dev/null && echo -ne "$("$_PYTHON" --version 2>&1 | awk '{print $2}' | tr -d ' \n')"
 }
 
 plib_python_major_minor_version(){
-  command -v python > /dev/null && echo -ne "$(python --version 2>&1 | awk -F '[. ]' '{printf("%s.%s",$2,$3)}')"
+  command -v "$_PYTHON" > /dev/null && echo -ne "$("$_PYTHON" --version 2>&1 | awk -F '[. ]' '{printf("%s.%s",$2,$3)}')"
 }
 
 plib_python_major_version(){
-  command -v python > /dev/null && echo -ne "$(python --version 2>&1 | awk -F '[. ]' '{print $2}')"
+  command -v "$_PYTHON" > /dev/null && echo -ne "$("$_PYTHON" --version 2>&1 | awk -F '[. ]' '{print $2}')"
 }
 
 plib_pyenv_version(){
