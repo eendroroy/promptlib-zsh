ab0ef8ab3fa5a6f714820eda816d68082bcc4624
Zachary Cutlip <uid000@gmail.com>
2024-09-05 10:10:41 -0700

    use a function instead of a varible to determine "python3" vs "python"

--- modules/python.zsh
+++ modules/python.zsh
@@ -1,28 +1,32 @@
 #!/usr/bin/env zsh
 
 # default is regular 'python'
-_PYTHON=python;
-if ! command -v python > /dev/null && command -v python3 > /dev/null;
-then
-    # if there *not* a 'python' but there *is* a 'python3', use that instead
-    # this is typical on macOS
-    _PYTHON=python3;
-fi
+
+__python_executable_name() {
+  if command -v python3 > /dev/null;
+  then
+      # if is a 'python3', use that instead
+      # this is typical on macOS
+      echo "python3"
+  else
+    echo "python"
+  fi
+}
 
 plib_venv(){
   [[ -n "${VIRTUAL_ENV}" ]] && echo -n "($(basename "$VIRTUAL_ENV"))"
 }
 
 plib_python_version(){
-  command -v "$_PYTHON" > /dev/null && echo -ne "$("$_PYTHON" --version 2>&1 | awk '{print $2}' | tr -d ' \n')"
+  command -v $(__python_executable_name) > /dev/null && echo -ne "$($(__python_executable_name) --version 2>&1 | awk '{print $2}' | tr -d ' \n')"
 }
 
 plib_python_major_minor_version(){
-  command -v "$_PYTHON" > /dev/null && echo -ne "$("$_PYTHON" --version 2>&1 | awk -F '[. ]' '{printf("%s.%s",$2,$3)}')"
+  command -v $(__python_executable_name) > /dev/null && echo -ne "$($(__python_executable_name) --version 2>&1 | awk -F '[. ]' '{printf("%s.%s",$2,$3)}')"
 }
 
 plib_python_major_version(){
-  command -v "$_PYTHON" > /dev/null && echo -ne "$("$_PYTHON" --version 2>&1 | awk -F '[. ]' '{print $2}')"
+  command -v $(__python_executable_name) > /dev/null && echo -ne "$($(__python_executable_name) --version 2>&1 | awk -F '[. ]' '{print $2}')"
 }
 
 plib_pyenv_version(){
